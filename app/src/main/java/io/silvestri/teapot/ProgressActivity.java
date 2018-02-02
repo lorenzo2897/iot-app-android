c9a8f1f7920c5ce3db1281d99eb202e7d14a0f74
Lorenzo Silvestri <code@silvestrix.org>
2018-02-02 00:08:36 +0000

    added progress activity

---
+++ app/src/main/java/io/silvestri/teapot/ProgressActivity.java
@@ -0,0 +1,13 @@
+package io.silvestri.teapot;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProgressActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_progress);
+	}
+}
